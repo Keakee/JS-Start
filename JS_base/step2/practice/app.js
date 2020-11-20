e1795d28eb78fd3d560d8959f7c686a5048040b3
Keakee <msk.timka@gmail.com>
2020-11-20 19:59:03 +0200

    for loops go woop

---
+++ JS_base/step2/practice/app.js
@@ -0,0 +1,3 @@
+const currency = require('../../../currency.js')
+console.log('50 CA$ -> US');
+console.log(currency.canDolToUS(30));
--- app.js
+++ app.js
@@ -1,3 +1,31 @@
-const currency = require('./currency.js')
-console.log('50 CA$ -> US');
-console.log(currency.canDolToUS(30));
+//connecting installed module through the require command
+let express = require('express');
+//creating new object экземпляр класса express
+let app = express();
+
+const port = 3000;
+const framework = 'express';
+app.use(express.static('public'));
+
+// Launch Templator
+app.set('view engine', 'pug');
+
+app.listen(3000, function(){
+    console.log(`node express working on ${port}`);
+});
+
+//Next, to launch the first app
+//get - метод  обращения
+
+app.get('/', function(request, response){
+    // response.end('<h1>Hello</h1> my first indie website');
+    response.render('main', {
+        port: port,
+        year: 2020,
+        framework: framework
+    });
+});
+
+// app.get('/categories', function(request, response){
+//     response.end('<h1>Categories</h1>');
+// });
